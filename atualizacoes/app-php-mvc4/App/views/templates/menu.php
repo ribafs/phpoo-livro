879f82d7cb6b77524db904fd54e8827ac19b50ec
Ribamar FS <ribafs@gmail.com>
2022-09-03 20:43:58 -0300

    Update menu.php

--- atualizacoes/app-php-mvc4/App/views/templates/menu.php
+++ atualizacoes/app-php-mvc4/App/views/templates/menu.php
@@ -1,8 +1,8 @@
 		<!-- MENU -->
 		<nav class="navbar-expand bg-secondary navbar-dark mt-0">
 			<ul class="navbar-nav justify-content-center">
-			  <li class="nav-item"><a class="nav-link link-light" href="<?php echo URL; ?>clients">Clientes</a></li>
-			  <li class="nav-item"><a class="nav-link ms-3 link-light" href="<?php echo URL; ?>products">Produtos</a></li>
+			  <li class="nav-item"><a class="nav-link link-warning" href="<?php echo URL; ?>clients">Clientes</a></li>
+			  <li class="nav-item"><a class="nav-link ms-3 link-warning" href="<?php echo URL; ?>products">Produtos</a></li>
 			</ul>
 		</nav>
 	</div>
